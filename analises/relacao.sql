1a45eb1436de73bf05facd21500625ad7f056967
Thales Luiz <thalesluizbraga@gmail.com>
2024-04-05 20:09:58 -0300

    relacao.sql finalizada v1

--- analises/relacao.sql
+++ analises/relacao.sql
@@ -6,20 +6,24 @@
 -- entregas canceladas sobre o total de entregas
 -- entregas de entregadores not match sobre o total de entregas canceladas
 
-
-select
-    count(order_id) as pd_canc_drivers_not_mach, 
-    (select count(order_id) from orders where order_status = 'CANCELLED') as total_ordens_canceladas,
-    count(order_id) * 1.0 /  
-    (select count(order_id) from orders where order_status = 'CANCELLED') as indice_cancelados_drivers_not_match
-    
+ with drivers_not_match as (
+    select
+        count(order_id) as ord_canc_drivers_not_match
 
 
-from orders AS o
-inner join drivers as d on o.Driver_ID = d.Driver_ID  
-inner join biometry as b on d.Driver_ID = b.Driver_ID
+    from orders AS o
+    inner join drivers as d on o.Driver_ID = d.Driver_ID  
+    inner join biometry as b on d.Driver_ID = b.Driver_ID
 
-where
-    o.Order_Status = 'CANCELLED'
-    
---- VOLTAR AQUI.... AINDA NAO ACABEI
+    where
+        o.Order_Status = 'CANCELLED'
+)
+
+select 
+    ord_canc_drivers_not_match,
+    (select count(order_id) from orders where order_status = 'CANCELLED') as ordens_canceladas,
+    ord_canc_drivers_not_match  * 1.0 /
+    (select count(order_id) from orders where order_status = 'CANCELLED') as indice
+
+from drivers_not_match as dnm
+
